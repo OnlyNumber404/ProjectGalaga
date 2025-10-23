4c58fa10b1a9ab1426bab0e5100c17f9376270e0
OnlyNumber404 <agustinusdenny110@gmail.com>
2025-10-23 23:09:35 +0700

    Menambah Objek Segitiga dan pergerakan

--- MainGUI.java
+++ MainGUI.java
@@ -1,19 +1,29 @@
 package Week_9;
 
+import Week_8_Lab.*;
 import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Event;
+
 import javax.swing.JButton;
 import java.awt.FlowLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MainGUI {
 
 	private JFrame frame;
+	private JPanel Gameplaypanel;
+	private Triangle triangle;
 
 	/**
 	 * Launch the application.
@@ -56,14 +66,62 @@ public class MainGUI {
 		ButtonPanel.setLayout(new BorderLayout(0, 0));
 		
 		JButton btnNewButton = new JButton("START");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int panelwidth = Gameplaypanel.getWidth();
+				int panelheight = Gameplaypanel.getHeight();
+				triangle = new Triangle();
+				triangle.setHeight(40);
+				triangle.setWidth(40);
+				triangle.setShapeColor(Color.green);
+				triangle.setXcoor((panelwidth - triangle.getWidth())/2);
+				triangle.setYcoor(panelheight - 100);
+				
+				Gameplaypanel.repaint();
+				
+				Gameplaypanel.requestFocusInWindow();
+			}
+		});
 		btnNewButton.setFont(new Font("Dialog", Font.PLAIN, 30));
 		btnNewButton.setBackground(new Color(192, 192, 192));
 		btnNewButton.setPreferredSize(new Dimension(80, 40));
 		ButtonPanel.add(btnNewButton);
 		
-		JPanel Gameplaypanel = new JPanel();
+		Gameplaypanel = new JPanel(){
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				if (triangle != null) {
+					triangle.UpdateTrianglePoints();
+					g.setColor(triangle.GetShapeColor());
+					g.fillPolygon(triangle.getXpoints(),triangle.getYpoints(),triangle.TRIANGLE_POINTS);
+					
+				}
+			}
+		};
+		Gameplaypanel.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (triangle != null) {
+					switch(e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						triangle.setXcoor(triangle.getXcoor()-10);
+						System.out.println("Key pressed: " + e.getKeyCode());
+						Gameplaypanel.repaint();
+						break;
+					case KeyEvent.VK_RIGHT:
+						triangle.setXcoor(triangle.getXcoor()+10);
+						System.out.println("Key pressed: " + e.getKeyCode());
+						Gameplaypanel.repaint();
+						break;				
+					}
+				}
+			}
+			  
+		});
 		Gameplaypanel.setBackground(new Color(255, 255, 255));
+		Gameplaypanel.setFocusable(true);
 		MainPanel.add(Gameplaypanel, BorderLayout.CENTER);
 	}
-
 }
