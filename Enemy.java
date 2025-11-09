54c2b387ba20f6c8c0a4c644e1ad2c42b4110f3f
OnlyNumber404 <agustinusdenny110@gmail.com>
2025-11-09 23:55:22 +0700

    Project Galaga Final V1.0

--- Enemy.java
+++ Enemy.java
@@ -1,50 +1,39 @@
 package Week_9;
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Rectangle;
+import Week_8_Lab.*;
 
 public class Enemy {
-    // posisi musuh (koordinat kiri atas)
-    private int x;
-    private int y;
-
-    // ukuran musuh (lebar dan tinggi sama, karena bentuknya kotak)
-    private int width;
-    private int height;
-
-    // warna musuh
-    private Color color;
-
-    // konstruktor buat bikin objek musuh baru
-    public Enemy(int x, int y, int size, Color color) {
-        this.x = x; // posisi awal X
-        this.y = y; // posisi awal Y
-        this.width = size; // ukuran lebar
-        this.height = size; // ukuran tinggi
-        this.color = color; // warna kotak musuh
+    
+    private Shape shape;
+    
+    public Enemy(Shape shape) {
+    	this.shape = shape;
     }
-
-    // fungsi buat gambar musuh di layar
-    public void draw(Graphics g) {
-        g.setColor(color); // atur warna
-        g.fillRect(x, y, width, height); // gambar kotak musuh
+    
+    public Shape getShape() {
+    	return shape;
     }
-
-    // fungsi buat mindahin posisi musuh (gerak ke kanan/kiri/bawah)
-    public void translate(int dx, int dy) {
-        this.x += dx; // geser di sumbu X
-        this.y += dy; // geser di sumbu Y
+    
+    
+    // ambil psosisi musuh
+    public int getX() {
+    	return shape.GetXCoord();
     }
-
-    // fungsi buat ambil area kotak musuh (dipakai buat deteksi tabrakan)
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
+    public int getY() {
+    	return shape.GetYCoord();
     }
-
-    // getter (pengambil data) posisi dan ukuran musuh
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public int getWidth() { return width; }
-    public int getHeight() { return height; }
+    public int getWidth() {
+    	return shape.GetWidth();
+    }
+    public int getHeight() {
+    	return shape.GetHeight();
+    }
+    
+    //buat gerakan musuh
+    //dx = geser ke kanan (+) atau ke kiri(-)
+    //dy = geser ke atas (+) atau ke bawah(-)
+    public void move (int dx,int dy) {
+    	shape.SetXCoord(shape.GetXCoord()+ dx);
+    	shape.SetYCoord(shape.GetYCoord()+ dy);
+    }
+    
 }
--- MainGUI.java
+++ MainGUI.java
@@ -15,25 +15,28 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import Text.MakeText;
 import Week_8_Lab.*;
-
+import Sound.SoundPlayer;
 
 public class MainGUI {
     private JFrame frame;
     private JPanel Gameplaypanel;
     private Triangle triangle; // objek pemain (pesawat segitiga)
-    private Rectangle rectangle; // objek peluru (Laser)
-
+//    private Rectangle rectangle; // objek peluru (Laser) one
+    private LinkedList<Rectangle> lasers = new LinkedList<>();
     private EnemyManager enemyManager; // mengatur musuhnya
 
     // flag input keyboard
     private boolean leftPressed = false;
     private boolean rightPressed = false;
-    private boolean spacePressed = false;
 
     // game loop timer (dipakai untuk update & repaint)
     private javax.swing.Timer gameTimer;
-
+    //untuk score
+    private int score;
+    //untuk text Game Over
+    private boolean lose = false;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -61,7 +64,7 @@ public class MainGUI {
      */
     private void initialize() {
         frame = new JFrame();
-        frame.setResizable(false);;
+        frame.setResizable(false);//agar ukuran frame tetap (tidak bisa berubah)
         frame.setBounds(100, 100, 450, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -80,7 +83,6 @@ public class MainGUI {
             public void mouseClicked(MouseEvent e) {
                 // pas tombol diklik: mulai game
                 startGame();
-                makelaser();
             }
         });
         btnNewButton.setFont(btnNewButton.getFont().deriveFont(30f));
@@ -91,21 +93,37 @@ public class MainGUI {
         Gameplaypanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
-                // custom drawing setiap repaint
                 super.paintComponent(g);
                 // gambar pemain (triangle)
                 if (triangle != null) {
-                    triangle.UpdateTrianglePoints(); // pastikan titik segitiga ter-update
+                    triangle.UpdateTrianglePoints(); //update posisi triangle
                     g.setColor(triangle.GetColor());
                     g.fillPolygon(triangle.GetXPoints(), triangle.GetYPoints(), triangle.TRIANGLE_POINTS);
                 }
-                if (spacePressed) {
-                	g.setColor(rectangle.GetColor());
-                	g.fillRect(rectangle.GetXCoord(), rectangle.GetYCoord(), rectangle.GetWidth(), rectangle.GetHeight());
+                //gamabar semua peluru dari linked List
+                Node<Rectangle> curr = lasers.GetLinkedListHead();
+                while (curr != null) {
+                	Rectangle laser = curr.GetNodeData();
+                	g.setColor(laser.GetColor());
+                	g.fillRect(laser.GetXCoord(),laser.GetYCoord(), laser.GetWidth(), laser.GetHeight());
+                	curr = curr.GetNext();
                 }
                 // gambar semua musuh lewat enemyManager
                 if (enemyManager != null) {
                     enemyManager.drawAll(g);
+                }
+                // untuk score
+                g.setColor(Color.black);
+                g.setFont(g.getFont().deriveFont(20f));
+                g.drawString("Score: " + score, 20, 30);
+                
+                //untuk Game Over
+                if (lose) {
+                	int x = (100);
+                	int y = (Gameplaypanel.getSize().height/2);
+                	g.setColor(Color.black);
+                	g.setFont(g.getFont().deriveFont(40f));
+                	g.drawString("GAME OVER",x ,y);
                 }
             }
         };
@@ -118,7 +136,8 @@ public class MainGUI {
                 if (e.getKeyCode() == KeyEvent.VK_LEFT) leftPressed = true;
                 else if (e.getKeyCode() == KeyEvent.VK_RIGHT) rightPressed = true;
                 else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    // kosong sekarang, bisa ditambahin buat menembak nanti
+                	SoundPlayer.playSound("src/laser-312360.wav");
+                	makelaser();
                 }
             }
 
@@ -127,7 +146,6 @@ public class MainGUI {
                 // reset flag pas tombol dilepas
                 if (e.getKeyCode() == KeyEvent.VK_LEFT) leftPressed = false;
                 else if (e.getKeyCode() == KeyEvent.VK_RIGHT) rightPressed = false;
-                else if (e.getKeyCode() == KeyEvent.VK_SPACE) spacePressed = true;
             }
         });
 
@@ -135,7 +153,6 @@ public class MainGUI {
         Gameplaypanel.setFocusable(true); // biar bisa nerima input keyboard
         MainPanel.add(Gameplaypanel, BorderLayout.CENTER);
         gameTimer = new javax.swing.Timer(16, ev -> {
-
             // update posisi pemain berdasarkan keyboard
             if (triangle != null) {
                 int panelW = Gameplaypanel.getWidth();
@@ -151,37 +168,73 @@ public class MainGUI {
                 }
             }
             
-            //update laser
-            if (rectangle != null) {
-            	int speed = 4;
-            	int newY = rectangle.GetYCoord();
-            	if (spacePressed) {
-            		newY-=speed;
-            	}
-            	rectangle.SetYCoord(newY);
+            //update posisi peluru
+            Node<Rectangle> curr = lasers.GetLinkedListHead();
+            Node<Rectangle> prev = null;
+            while (curr != null) {
+            	Rectangle laser = curr.GetNodeData();
+            	int newY = laser.GetYCoord()-10;
+            	laser.SetYCoord(newY);
             	
+            	if (newY < 0) {
+            		if (prev == null) {
+            			lasers = removeHead(lasers);
+            			curr = lasers.GetLinkedListHead();
+            			System.out.println("Remove laser");
+            			continue;
+            			} else {
+            				prev.SetNext(curr.GetNext());
+            		}
+            	}else {
+            		prev = curr;
+            		curr = curr.GetNext();
+            	}
+            }
+            
+            Node<Rectangle> laserNode = lasers.GetLinkedListHead();
+            while (laserNode != null) {
+            	Rectangle laser = laserNode.GetNodeData();
+            	
+            	Node<Shape> enemyNode = enemyManager.getEnemyList().GetLinkedListHead();
+            	boolean hit = false;
+            	
+            	while (enemyNode != null) {
+            		Shape enemy = enemyNode.GetNodeData();
+            		
+            		if (isColl(laser,enemy)) {
+            			lasers.RemoveNodeByData(laser);
+            			enemyManager.getEnemyList().RemoveNodeByData(enemy);
+            			SoundPlayer.playSound("src/080884_bullet-hit-39872.wav");
+            			score += 1;
+            			System.out.println("Enemy Hit");
+            			hit = true;
+            			break;
+            		}
+            		enemyNode = enemyNode.GetNext();
+            	}
+            	if (hit) break;
+            	laserNode = laserNode.GetNext();
             }
 
-            // update musuh
-            if (enemyManager != null) {
-                enemyManager.update(Gameplaypanel.getWidth());
-
-                // cek collision antara player polygon dan semua enemynya
-                if (triangle != null) {
-                    Polygon playerPoly = new Polygon(triangle.GetXPoints(), triangle.GetYPoints(), triangle.TRIANGLE_POINTS);
-                    if (enemyManager.checkCollisionWithPolygon(playerPoly)) {
-                        // game over kalau ada yang nabrak player
-                        gameTimer.stop();
-                        System.out.println("Game Over: musuh nabrak pesawatmu");
-                    }
-                    // tambahan: kalo ada musuh yang sampai ke level y player  game over
-                    if (enemyManager.anyReachedY(triangle.GetYCoord())) {
-                        gameTimer.stop();
-                        System.out.println("Game Over: musuh sudah mencapai level ");
-                    }
-                }
+            // cek tabrakan anatar musuh dan pesawat
+            if (triangle != null && enemyManager != null) {
+            	Polygon playerPoly = new Polygon(triangle.GetXPoints(),triangle.GetYPoints(),triangle.TRIANGLE_POINTS);
+            	
+            	// kalo musuh nabarak pesawat
+            	if (enemyManager.checkCollisionWithPolygon(playerPoly)) {
+            		gameTimer.stop();
+            		enemyManager.stopAll();
+            		lose = true;
+            		System.out.println("Game OVER");
+            	}
+            	//kalo musuh udah sampai posisi pesawat
+            	if (enemyManager.anyReachedY(triangle.GetYCoord())) {
+            		gameTimer.stop();
+            		enemyManager.stopAll();
+            		lose = true;
+            		System.out.println("Game OVER");
+            	}
             }
-
             // repaint panel tiap frame
             Gameplaypanel.repaint();
         });
@@ -195,20 +248,19 @@ public class MainGUI {
 
         // inisialisasi objek pemain/player 
         triangle = new Triangle();
-        triangle.SetWidth(40);
-        triangle.SetHeight(40);
+        triangle.SetWidth(20);
+        triangle.SetHeight(20);
         triangle.SetColor(Color.green);
         triangle.SetXCoord((panelwidth - triangle.GetWidth()) / 2); // mulai di tengah layar
         triangle.SetYCoord(panelheight - 100); // posisi Y agak di atas bawah panel
         triangle.UpdateTrianglePoints(); // set titik polygon
         
         // inisialisasi musuh
-        int leftMargin = 20; // jarak kiri dari edge
-        int topY = 20; // posisi Y paling atas musuh
-        int count = 5; // jumlah musuh di baris pertama (bisa disesuaikan)
-        enemyManager = new EnemyManager(count, leftMargin, topY, Triangle.BASE_SIZE);
+        int leftMargin = 20;
+        int topY = 20;
+        enemyManager = new EnemyManager(15, leftMargin, topY, Triangle.BASE_SIZE,Gameplaypanel);
 
-        // minta fokus ke panel supaya keyboard bisa dipakai tanpa klik lagi
+        // minta fokus ke panel
         Gameplaypanel.requestFocusInWindow();
     }
     
@@ -217,11 +269,43 @@ public class MainGUI {
     	int panelheight = Gameplaypanel.getWidth();
     	
     	// inisialisasi
-    	rectangle = new Rectangle();
-    	rectangle.SetWidth(5);
-    	rectangle.SetHeight(40);
-    	rectangle.SetColor(Color.red);
-    	rectangle.SetXCoord(triangle.GetXCoord() + (triangle.GetWidth()/2)-3);
-    	rectangle.SetYCoord(triangle.GetYCoord() - 40);
+    	Rectangle laser = new Rectangle();
+    	laser.SetWidth(5);
+    	laser.SetHeight(30);
+    	laser.SetColor(Color.red);
+    	laser.SetXCoord(triangle.GetXCoord() + (triangle.GetWidth()/2)-3);
+    	laser.SetYCoord(triangle.GetYCoord() - 40);
+    	lasers.AddNode(new Node<>(laser));
+    }
+    
+    private LinkedList<Rectangle> removeHead(LinkedList<Rectangle> list){
+    	Node<Rectangle> head = list.GetLinkedListHead();
+    	if (head != null) {
+    		list = new LinkedList<>();
+    		Node<Rectangle> next = head.GetNext();
+    		while (next != null) {
+    			list.AddNode(new Node<>(next.GetNodeData()));
+    			next = next.GetNext();
+    		}
+    	}
+    	return list;
+    }
+    //coll antara laser dan enemy
+    private boolean isColl(Rectangle laser,Shape enemy) {
+    	//ambil enemy hit box
+    	int ex = enemy.GetXCoord();
+    	int ey = enemy.GetYCoord();
+    	int ew = enemy.GetWidth();
+    	int eh = enemy.GetHeight();
+    	
+    	//ambil laser hit box
+    	int lx = laser.GetXCoord();
+    	int ly = laser.GetYCoord();
+    	int lw = laser.GetWidth();
+    	int lh = laser.GetHeight();
+    	
+    	int margin = 5;
+    	
+    	return lx < ex + ew - margin && lx + lw > ex + margin && ly < ey + eh - margin && ly + lh > ey + margin; 
     }
 }
